a9ad36176b4a95de198a24c80fe61a052d98f261
Abdelmonem28 <poody200412@gmail.com>
2024-06-18 15:42:20 +0300

    feat: Improve bundler function to handle imports and generate bundle

--- nebula/src/cli/compile.ts
+++ nebula/src/cli/compile.ts
@@ -1,8 +1,16 @@
 import { exec } from "child_process";
 import listFiles from "./ListFiles";
 import fs from "fs";
+import bundler from "./bundler";
+import path from "path";
 
-export default async function compile(distFolder: string): Promise<void>{
+export default async function compile(distFolder: string): Promise<void> {
         const files = await listFiles("./app/pages");
-        fs.writeFileSync(`${distFolder}/index.ts`, '');
+        for (const file of files) {
+                const folder = path.dirname(file.replace('app/pages', ''));
+                const distfile = path.join(distFolder, file.replace('app/pages', ''));
+                if (!fs.existsSync(path.join(distFolder, folder)))
+                        fs.mkdirSync(path.join(distFolder, folder), { recursive: true });
+                fs.writeFileSync(distfile, `${bundler(file)}\n`, { flag: 'a' });
+        }
 }
